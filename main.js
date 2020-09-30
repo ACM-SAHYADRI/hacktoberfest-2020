e455defb5bd0babd4a06e5a9d9d49165c34236e8
luciferRK <likhithr.cs17@sahyadri.edu.in>
2020-09-30 22:11:48 +0530

    initializing

---
+++ main.js
@@ -0,0 +1,59 @@
+let contributers=[
+    {
+        "name":"Likhith RK",
+        "college":"SCEM",
+        "image":"images/likhith.jpg",
+        "github":"https://github.com/luciferRK",
+        "linkedin":"https://linkedin.com/in/likhith-rk",
+        "instagram":"https://instagram.com/lucifer_rk_"
+    },
+]
+
+
+
+
+
+
+//_________________________________________________________________________________________________________________
+
+//******************************************DO NOT CHANGE THE BELOW LINES******************************************
+//_________________________________________________________________________________________________________________
+
+function getPerson(person){
+    let divi=document.createElement('div');
+    divi.className="col-md-3 col-sm-6 col-xs-12 wow fadeIn";
+    divi.setAttribute("data-wow-offset","50");
+    divi.setAttribute("data-wow-delay","1.3s");
+    divi.innerHTML= '<div class="team-wrapper"><img src="'+person.image+'" class="img-responsive" alt="team img 1"><div class="team-des"><h4>'+person.name+'</h4><span>'+person.college+'</span><ul class="social-icon"><li><a href="'+person.linkedin+'" target="_blank" target="_blank" class="fa fa-linkedin"></a></li><li><a href="'+person.github+'" target="_blank" class="fa fa-github"></a></li><li><a href="'+person.instagram+'" target="_blank"  class="fa fa-instagram"></a></li></ul></div></div>';
+    return divi;
+}
+
+function fill(){
+    let n=contributers.length;
+    let container=document.getElementById('all_contributers');
+    let row=document.createElement('div');
+    row.className="row";
+
+    let div=getPerson(contributers[0]);
+    row.append(div);
+
+    for(var i=1;i<n+4;i++){
+        if (i>=n){
+            container.append(row);
+            break;
+        }
+        if(i%4==0){
+            container.append(row);
+            row=document.createElement('div');
+            row.className="row";
+            div=getPerson(contributers[i]);
+            row.append(div);
+        }
+        else{
+            div=getPerson(contributers[i]);
+            row.append(div);
+        }
+    }
+}
+
+window.onload=fill;

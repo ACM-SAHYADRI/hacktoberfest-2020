5e02bad2d34d26f9c1ee3bce6484e34ac082084d
Likhith R K <48903317+luciferRK@users.noreply.github.com>
2020-10-01 21:30:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -185,7 +185,7 @@ function getPerson(person){
     divi.className="col-md-3 col-sm-6 col-xs-12 wow fadeIn";
     divi.setAttribute("data-wow-offset","50");
     divi.setAttribute("data-wow-delay","1.3s");
-    divi.innerHTML= '<div class="team-wrapper"><img src="'+person.image+'" class="img-responsive" alt="team img 1"><div class="team-des"><h4>'+person.name+'</h4><span>'+person.college+'</span><ul class="social-icon"><li><a href="'+person.linkedin+'" target="_blank" target="_blank" class="fa fa-linkedin"></a></li><li><a href="'+person.github+'" target="_blank" class="fa fa-github"></a></li><li><a href="'+person.instagram+'" target="_blank"  class="fa fa-instagram"></a></li></ul></div></div>';
+    divi.innerHTML= '<div class="team-wrapper"><img src="'+person.image+'" class="img-responsive" alt="User_image"><div class="team-des"><h4>'+person.name+'</h4><span>'+person.college+'</span><ul class="social-icon"><li><a href="'+person.linkedin+'" target="_blank" target="_blank" class="fa fa-linkedin"></a></li><li><a href="'+person.github+'" target="_blank" class="fa fa-github"></a></li><li><a href="'+person.instagram+'" target="_blank"  class="fa fa-instagram"></a></li></ul></div></div>';
     return divi;
 }
 

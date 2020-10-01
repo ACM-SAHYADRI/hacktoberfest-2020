fac838c9fac6f4e1ff4efd66b874540a1f747837
Likhith R K <48903317+luciferRK@users.noreply.github.com>
2020-10-01 13:42:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -77,7 +77,7 @@ let contributers=[
     },
     {
       "name":"Arkadyuti Bandyopadhyay",
-      "college":"RCC Institute of IT",
+      "college":"RCC Institute of Information Technology",
       "image":"images/Arkadyuti_Bandyopadhyay.jpg",
       "github":"https://github.com/ARKEOLOGIST",
       "linkedin":"https://www.linkedin.com/in/arkadyuti-bandyopadhyay-a8a965177/",
@@ -106,7 +106,7 @@ let contributers=[
         "github":"https://github.com/shamithag131",
         "linkedin":"https://www.linkedin.com/in/shamitha-g-9b4198181/",
         "instagram":""
-      }
+    }
 ]
 
 

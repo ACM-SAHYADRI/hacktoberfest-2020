fb9793ccc1f50a76b0d5ced4e51eb203d38eb7ce
Prakyath17 <72190205+Prakyath17@users.noreply.github.com>
2020-10-01 18:24:59 +0530

    Update main.js

--- main.js
+++ main.js
@@ -138,7 +138,15 @@ let contributers=[
         "github":"https://github.com/sukanyashet",
         "linkedin":"https://www.linkedin.com/in/sukanya-shet-s-465271181/",
         "instagram":"https://www.instagram.com/su_kan_yaa",
-     }
+     }, 
+     {
+        "name":"Prakyath H N", 
+        "college":" SCEM", 
+        "image": "Image_prakyath.jpeg", 
+        "github":"https://github.com/Prakyath17", 
+        "linkedin":"https://www.linkedin.com/in/prakyath-h-n-9539b11b8", 
+        "instagram":"https://instagram.com/prakii_boy?igshid=1a00ogsbjhnre", 
+     }, 
 ]
 
 

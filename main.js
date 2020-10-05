9f344ea33887f10635c217336cd072f053d4653a
Sreenidhi <47133316+Sree27-Shanger@users.noreply.github.com>
2020-10-05 13:29:55 +0530

    Updated main.js

--- main.js
+++ main.js
@@ -393,6 +393,14 @@ let contributers=[
         "github":"https://github.com/LumaLiana",
         "linkedin":"https://www.linkedin.com/in/luma-bueno-556531157/",
         "instagram":"https://instagram.com/luma_liana"
+    },
+	{
+        "name":"Sreenidhi",
+        "college":"SCEM",
+        "image":"images/sreenidhi.jpg",
+        "github":"https://github.com/Sree27-Shanger",
+        "linkedin":"",
+        "instagram":""
     }
  ];
 

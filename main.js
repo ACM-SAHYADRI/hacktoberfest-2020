947e8dbf5704d6a017f9fa6a3e194feda7ec4e4a
Waqar Ansari <40629788+mdwaqaransari@users.noreply.github.com>
2020-10-17 19:41:03 +0530

    details added

--- main.js
+++ main.js
@@ -442,6 +442,14 @@ let contributers=[
         "linkedin":"https://www.linkedin.com/in/sudharshan-bhimisetty-19a8241aa/",
         "instagram":"https://www.instagram.com/siddu__24/"
     }	
+    {
+        "name":"Waqar Ansari",
+        "college":"Madan Mohan Malviya University ofTechnology, Gorakhpur",
+        "image":"images/waqar.jpg",
+        "github":"https://github.com/mdwaqaransari",
+        "linkedin":"https://www.linkedin.com/in/mohd-waqar-ansari-75b15618b/",
+        "instagram":"https://www.instagram.com/md_waqaransari/"
+    }
  ];
 
 
